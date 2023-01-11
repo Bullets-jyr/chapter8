c1bbf51ccfc44c0d1248a4b2524ad62cad36067b
Bullets-jyr <jyr@bullets.co.kr>
2023-01-12 07:18:33 +0900

    05. 액자 UI 그리기 (2)

--- app/src/main/java/kr/co/bullets/chapter8/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/chapter8/MainActivity.kt
@@ -1,6 +1,7 @@
 package kr.co.bullets.chapter8
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
@@ -31,7 +32,19 @@ class MainActivity : AppCompatActivity() {
             checkPermission()
         }
 
+        binding.navigateFrameActivityButton.setOnClickListener {
+            navigateToFrameActivity()
+        }
+
         initRecyclerView()
+    }
+
+    private fun navigateToFrameActivity() {
+        val images = imageAdapter.currentList.filterIsInstance<ImageItems.Image>().map {
+            it.uri.toString()
+        }.toTypedArray()
+        val intent = Intent(this, FrameActivity::class.java).putExtra("images", images)
+        startActivity(intent)
     }
 
     private fun initRecyclerView() {
