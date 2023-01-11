4186f102e7e105ffe11a96368e6f01b47142fe55
Bullets-jyr <jyr@bullets.co.kr>
2023-01-11 16:36:22 +0900

    03. 갤러리에서 이미지 불러오기

--- app/src/main/java/kr/co/bullets/chapter8/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/chapter8/MainActivity.kt
@@ -2,9 +2,12 @@ package kr.co.bullets.chapter8
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -13,6 +16,9 @@ import kr.co.bullets.chapter8.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val imageLoadLauncher = registerForActivityResult(ActivityResultContracts.GetMultipleContents()) { uriList ->
+        updateImages(uriList)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,11 +58,32 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadImage() {
-        Toast.makeText(this, "이미지를 가져올 예정", Toast.LENGTH_SHORT).show()
+//        Toast.makeText(this, "이미지를 가져올 예정", Toast.LENGTH_SHORT).show()
+        imageLoadLauncher.launch("image/*")
     }
 
     private fun requestReadExternalStorage() {
         ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_READ_EXTERNAL_STORAGE)
+    }
+
+    private fun updateImages(uriList: List<Uri>) {
+        Log.i("updateImages", "$uriList")
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            REQUEST_READ_EXTERNAL_STORAGE -> {
+                if (grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED) {
+                    loadImage()
+                }
+            }
+        }
     }
 
     companion object {
